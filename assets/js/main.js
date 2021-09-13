9586e69229125e7f6748a19c734fc4facc65291c
ear19191919 <89884103+ear19191919@users.noreply.github.com>
2021-09-13 10:28:53 +0900

    問題の答え

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '米農家',
         },
         stage2: {
-          q1: 'いいい',
+          q1: '天使',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: '王',
           // q2: 'かかか',
           // q3: 'ききき',
         }
